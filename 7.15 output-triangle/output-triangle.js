67776f0169a4de1b559ee704dd9dba81f9de53fc
Vinceline <131508360+Vinceline@users.noreply.github.com>
2024-07-09 14:36:50 -0500

    Update output-triangle.js

--- 7.15 output-triangle/output-triangle.js
+++ 7.15 output-triangle/output-triangle.js
@@ -1,6 +1,9 @@
+//function to print a triangle of stars
 function drawTriangle(triangleSize) {
 let i = 0;
 let sub = "*"
+ //print a star and increment the number of stars
+ //until you reach the limit (triangle size)
  while ( i < triangleSize){
     console.log(sub)
     sub=sub.concat("*")
